7564b62e64aa633872067a8e20f658bbb232a63b
AJILEYE AYOKUNLE J <kunlesymls@hotmail.com>
2017-06-17 18:06:49 +0100

    Add project files.

---
+++ CompunetCbte/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CompunetCbte.Startup))]
+namespace CompunetCbte
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
